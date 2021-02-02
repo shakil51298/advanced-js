5cb58705f29f9d958f2856164f0489d0a8d6c81e
shakil51298 <zikaiwen02@gmail.com>
2021-02-03 00:49:17 +0800

    undefined vs null cleared

---
+++ undefined-vs-null.js
@@ -0,0 +1,27 @@
+// when the output would be undefined
+
+let birds;                       // ---> 1
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}                                //---> 2
+const result = add(13,82);
+console.log(result);
+
+function add(num1, num2){
+    console.log(num1,num2);
+}                                // ----> 3
+const sum = add(13);
+console.log(sum);
+
+const lover = {name: "shakil", phone: 58587932};
+console.log(lover.gf);           //---> 4
+
+let fun = undefined;
+console.log(fun);                   //---> 5
+
+   //null is Speicial thing(if i set a value of an property null only this the output wold be null)
+
+
+let ages = [12,21,5]                 //----6
+console.log(ages[11]);
